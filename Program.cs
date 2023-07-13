ca5bf80515506d852a57b1384f438360388f1c8a
unknown <kbhoomika05@gmail.com>
2023-07-13 14:18:48 +0530

    Assignment4

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num>0)
+            {
+                Console.WriteLine("The Number is Positive");
+            } else if(num == 0)
+            {
+                Console.WriteLine("The Number is Zero");
+            } else 
+            { 
+                Console.WriteLine("The Number is Negative"); 
+            }
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("The Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Odd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
